cace888878f0202f31152a33e2eb677d547b9cc1
Byuntil <gugugu2014@jbnu.ac.kr>
2021-04-06 01:04:35 +0900

    .

--- week2/week2.cpp
+++ week2/week2.cpp
@@ -4,16 +4,15 @@ using namespace std;
 int main() {
 	int N; //N장의 카드
 	int M; //딜러의 외침
-	int sum = 0, result = 0;
+	int sum = 0; //세 수의 합
+	int result = 0; //최대값
 	int arr[100] = { 0 };
 
 	cin >> N >> M;
 
 	for (int i = 0; i < N; i++)
 	{
-		int a = 0;
-		cin >> a;
-		arr[i] = a;
+		cin >> arr[i];
 	}
 
 	for (int i = 0; i < N; i++)
